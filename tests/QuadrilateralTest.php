7e11108f8ca8c1fcf7e15e2abbb1573734a3a610
Jonathon Klem <jklem@abbadon.local>
2015-09-12 21:05:04 -0500

    Add additional tests

--- tests/QuadrilateralTest.php
+++ tests/QuadrilateralTest.php
@@ -8,6 +8,28 @@ class QuadrilateralTest extends PHPUnit_Framework_TestCase
         $this->isInstanceOf('Quadrilateral',$instance);
         $this->assertEquals($instance->getArea(), 6);
     }
+
+		function testSetHeight() {
+				// defaults to 1x1
+        $instance = new Quadrilateral();
+
+				// should now be 1x5
+				$instance->setHeight(5);
+
+        $this->isInstanceOf('Quadrilateral',$instance);
+        $this->assertEquals($instance->getArea(), 5);
+		}
+
+		function testSetWidth() {
+				// defaults to 1x1
+        $instance = new Quadrilateral();
+
+				// should now be 7x1
+				$instance->setWidth(7);
+
+        $this->isInstanceOf('Quadrilateral',$instance);
+        $this->assertEquals($instance->getArea(), 7);
+		}
 }
 
 ?>
